e7c8dc6c7eea932d636833d0e79dd1312d197a4d
roBoot <maksim.nemkov.ldt@gmail.com>
2025-07-03 17:40:10 +0300

    Git commands

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-print("Just studying GitHub")
-print("This is a change to the main branch")
-print("Another changes into the main branch")
-print('Changes into the br2 branch')
+'''# Git Commands Cheat Sheet
+git tag <name> - tag the current commit
+git tag -l - list all tags
+git tag -d <name> - delete a tag
+git push origin <name> - push a tag to the remote repository
+git push origin --tags - push all tags to the remote repository
+git show <name> - show the details of a tag
+git checkout <name> - switch to a branch
+git checkout -b <name> - create a new branch and switch to it
+git branch -a - list all branches, including remote branches
+git branch -d <name> - delete a branch
+git branch -D <name> - force delete a branch
+git merge <name> - merge a branch into the current branch
+'''
